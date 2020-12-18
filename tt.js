059f57dd289d29f559b4b7a4e73c2231cc2681a9
Mark Simoncic <63131950+masimoncic@users.noreply.github.com>
2020-12-18 00:04:15 -0500

    main functionality complete. TODO: Order of operations and negation

--- tt.js
+++ tt.js
@@ -16,7 +16,7 @@ let conditionalProps = [];
 let biconditionalProps = [];
 let complexProps = [];
 let complexPropsHolder = [];
-let complexPropsColumns = [];
+let veryComplexProps = [];
 let allProps = [];
 let numRows;
 let logicSyms = ['~','&','|','->','=='];
@@ -58,11 +58,11 @@ function addInputsToArray(n) {
 }
 
 //after calling addInputsToArray, all props will be in inputProps
-// We need to divide them into basic and complex props
+// We will be dividing props into three arrays.  basicProps (no logical symbols), complexProps(1 logical symbol) and veryComplexProps(more than 1 logical symbol)
 
 
-
-function checkLogicSyms (str, sym) {
+//first we separate the basic from the non basic props.  non basic props go into complexPropsHolder, where they will be again partitioned into 2 arrays.
+function checkLogicSyms (str) {
     if  (logicSyms.some(substring=>str.includes(substring))){
         complexPropsHolder.push(str);
     }
@@ -71,74 +71,102 @@ function checkLogicSyms (str, sym) {
     }
 }
 
-//now all basic props are in basicProps, and complex props are in complexPropHolder
-//we want to break up complex props that have more than 1 logical symbol
-
-
-//start with just &
-
-
 function partitionProps() {
     inputProps.forEach(element => checkLogicSyms(element));
 }
 
+//now all user-inputed basic props are in basicProps, and complex props are in complexPropHolder
+//we want to break up complex props into our three arrays.  We need to add any basic props that show up in complex props (but were not input by the user themselves); 
+//we also want to break down the complex props into props with only 1 logical symbol: these will go into the complexProps array.  props with more than 1 symbol will remain stored in complexPropHolder
 
-function checkMultiConj (str) {
-    console.log(str);
-    if (!complexProps.includes(str)) {
-    let strMod = str.replace('&', '')
-        if (!strMod.includes('&')) {
-            complexProps.push(str);
-            conjunctionProps.push(str);
-        }
-        else {  
-            let strSlice1 = str.slice(0,3);
-            let strSlice2 = str.slice(2);
-            if (!complexProps.includes(strSlice1)) {
-                complexProps.push(str.slice(0,3));
-            }
-            if (!complexPropsHolder.includes(strSlice2)) {
-                complexPropsHolder.push(str.slice(2));
-            }
-        }
-        console.log(complexProps);
-        console.log(complexPropsHolder);
+
+
+
+
+
+//we will be looping breakUpComplexProps through complexPropHolder.  This function will break complex props into 3 substrings: 1. the leftmost basic prop in that complex prop. 
+//2. the leftmost combination which forms a complex prop with 1 logical symbol.   
+//3.  everything to the right of the first logical symbol (for negation, it will be to the right of the proposition)
+//The last substring will be pushed to the end of complexPropHolder to be looped over again. 
+
+
+//define slicePusher helper function
+function slicePusher(strSlice1, strSlice2, strSlice3) {
+    if (!basicProps.includes(strSlice1)) {
+        basicProps.push(strSlice1);
     }
-}
-
-function checkMultiConjAll() {
-    for (i=0; i<complexPropsHolder.length; i++) {
-        checkMultiConj(complexPropsHolder[i]);
+    if (!complexProps.includes(strSlice2)) {
+        complexProps.push(strSlice2);
     }
-}
-
-function createComplexPropsColumnsConj() {
-    for (i=0; i<complexPropsHolder.length; i++) {
-        if (!complexProps.includes(complexPropsHolder[i])) {
-            complexPropsColumns.push(complexPropsHolder[i]);
+    if (!complexPropsHolder.includes(strSlice3)) {
+        if (!(strSlice3 === '')) {
+            complexPropsHolder.push(strSlice3);
         }
     }
-    complexPropsColumns.sort(function (a,b) { return (a.length - b.length)});
 }
     
-function getAllBasicPropsConj () {
-    for (i=0; i<complexProps.length; i++) {
-        let s0 = complexProps[i][0];
-        let s2 =complexProps[i][2];
-        if (!basicProps.includes(s0)) {
-            basicProps.push(s0);
-        }
-        if (!basicProps.includes(s2)) {
-            basicProps.push(s2);
-        }
-    }
+function breakUpComplexProps () {
+    //check if it is a basic or complex prop.  This is needed because basic props will be added into the loop (below) as it breaks up complex props
+    for (i=0; i<complexPropsHolder.length; i++) {
+        str = complexPropsHolder[i];
+        if (!logicSyms.some(substring => str.includes(substring))) {
+            if (!basicProps.includes(str)) {
+                basicProps.push(str);
+            }
+        } 
+        //if str is not basic
+        else {
+            //if str is already in complexProps, do nothing
+            if (!complexProps.includes(str)) {
+                //now we check for negation
+                //currently negation is not working as intended
+                //eval negation last?
+                if (str[0] === '~') {
+                    let strSlice1 = str[1];
+                    let strSlice2 = str.slice(0,2);
+                    let strSlice3 = str.slice(1);
+                    slicePusher(strSlice1, strSlice2, strSlice3);
+                }
+                //check &, |
+                else if (str[1] === '&' || str[1] === '|') {
+                    let strSlice1 = str[0];
+                    let strSlice2 = str.slice(0,3);
+                    let strSlice3 = str.slice(2);
+                    slicePusher(strSlice1, strSlice2, strSlice3);
+                }
+                //check -> and ==
+                else if (str.slice(1,3) === '->' || str.slice(1,3) === '==') {
+                    let strSlice1 = str[0];
+                    let strSlice2 = str.slice(0,4);
+                    let strSlice3 = str.slice(3);
+                    slicePusher(strSlice1, strSlice2, strSlice3);
+                }
+            }
+        }  
+    }         
 }
 
 
 
 
-//placeholder
 
+
+
+
+//Now we need to create an array for all the complex props with more than 1 logical symbol
+
+function createVeryComplexProps() {
+    for (i=0; i<complexPropsHolder.length; i++) {
+        if (!complexProps.includes(complexPropsHolder[i])  && !basicProps.includes(complexPropsHolder[i])) {
+            veryComplexProps.push(complexPropsHolder[i]);
+        }
+    }
+    veryComplexProps.sort(function (a,b) { return (a.length - b.length)});
+}
+    
+
+
+//create columns
 
 function createPropColumn(i) {
     let th = document.createElement('th');
@@ -154,7 +182,7 @@ function createAllPropColumns() {
 }
 
 
-
+//create rows
 
 function createRows() {  
     for (i=1; i<= numRows; i++) {
@@ -165,7 +193,7 @@ function createRows() {
 
 }
 
-
+//define helper functions
 function getRow(rowNum) {
     return document.getElementById(`row${rowNum}`)
 
@@ -178,7 +206,12 @@ function createTd (c, r, val) {
     getRow(r).appendChild(td);
 
 }
+function getTd(c, r) {
+    return document.getElementById(`td${c}${r}`)
+}
 
+
+//start filling rows
 function fillAllBasicPropRows(n) {
     for (i=1; i < n+1; i++) {
         console.log(i);
@@ -196,9 +229,7 @@ function fillAllBasicPropRows(n) {
 }
 
 
-function getTd(c, r) {
-    return document.getElementById(`td${c}${r}`)
-}
+//helper functions
 function evalConj(td1, td2) {
     if (td1.innerHTML * td2.innerHTML === 1) {
         return 1;
@@ -207,12 +238,61 @@ function evalConj(td1, td2) {
     }
 }
 
+function evalNeg(td1) {
+    // td1.innerHTML === 0 doesn't work?
+    if (td1.innerHTML < 1) {
+        return 1;
+    } else{
+        return 0;
+    }
+}
 
+function evalDisj(td1, td2) {
+    if(td1.innerHTML + td2.innerHTML > 0) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
 
+function evalCond(td1, td2) {
+    if((td1.innerHTML > 0 ) && (td2.innerHTML < 1)) {
+        return 0;
+    } else {
+        return 1;
+    }
+}
+
+function evalBicond(td1, td2) {
+    if(td1.innerHTML === td2.innerHTML) {
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
+
+//finish filling rows
 function fillComplexRows () {
     for (i=0; i<allProps.length; i++) {
+
         //here will need to code an else if for every symbol
-        if (allProps[i][1] === '&') {
+        //negation
+        if (allProps[i][0] === '~') {
+            let temp1 = allProps[i][1];
+            let p1Col = allProps.indexOf(temp1) +1;
+            for (j=1; j<numRows +1; j++) {
+                td1 = getTd(p1Col, j);
+                td = document.createElement('td');
+                tdVal = evalNeg(td1);
+                td.innerHTML = tdVal;
+                td.id = `td${i+1}{j}`;
+                getRow(j).appendChild(td);
+            }
+        }
+
+        //conjunction
+        else if (allProps[i][1] === '&') {
             //temp gets the first character of the proposition, and p1Col gives its column (which will be used to find the table element by id)
             let temp1 = allProps[i][0];
             let p1Col = allProps.indexOf(temp1) +1;
@@ -230,40 +310,76 @@ function fillComplexRows () {
             }
 
         }
+
+        //disjunction
+        else if (allProps[i][1] === '|') {
+            let temp1 = allProps[i][0];
+            let p1Col = allProps.indexOf(temp1) +1;
+            let temp2 = allProps[i].slice(2);
+            let p2Col = allProps.indexOf(temp2) + 1;
+            for (j=1; j< numRows +1; j++) {
+                td1 = getTd(p1Col, j);
+                td2 = getTd(p2Col, j);
+                td = document.createElement('td');
+                tdVal = evalDisj(td1, td2);
+                td.innerHTML = tdVal;
+                td.id = `td${i+1}${j}`
+                getRow(j).appendChild(td);
+            }
+
+        }
+
+        //conditional 
+        else if (allProps[i][1] === '-') {
+            let temp1 = allProps[i][0];
+            let p1Col = allProps.indexOf(temp1) +1;
+            let temp2 = allProps[i].slice(3);
+            let p2Col = allProps.indexOf(temp2) + 1;
+            for (j=1; j< numRows +1; j++) {
+                td1 = getTd(p1Col, j);
+                td2 = getTd(p2Col, j);
+                td = document.createElement('td');
+                tdVal = evalCond(td1, td2);
+                td.innerHTML = tdVal;
+                td.id = `td${i+1}${j}`
+                getRow(j).appendChild(td);
+            }
+
+        }
+        //biconditional
+        else if (allProps[i][1] === '=') {
+            let temp1 = allProps[i][0];
+            let p1Col = allProps.indexOf(temp1) +1;
+            let temp2 = allProps[i].slice(3);
+            let p2Col = allProps.indexOf(temp2) + 1;
+            for (j=1; j< numRows +1; j++) {
+                td1 = getTd(p1Col, j);
+                td2 = getTd(p2Col, j);
+                td = document.createElement('td');
+                tdVal = evalBicond(td1, td2);
+                td.innerHTML = tdVal;
+                td.id = `td${i+1}${j}`
+                getRow(j).appendChild(td);
+            }
+
+        }
+        
+
     }
 }
 
-/*
-function fillRowsConj(c1, c2, c3) {
-    for (i=1; i<=numRows; i++) {
-            td1 = getTd(c1, i);
-            td2 = getTd(c2, i);
-            td = document.createElement('td');
-            tdVal = evalConj(td1, td2);
-            td.innerHTML = tdVal;
-            td.id = `td${c3}${i}`;
-            getRow(i).appendChild(td);
-
-        }
-}
-*/
 
 
 
 
 
-
-
-//bottom of page
 function htmlGenerateTableListener () {
     addInputsToArray(htmlInputButtonCount);
     partitionProps();
-    checkMultiConjAll();
-    getAllBasicPropsConj();
-    createComplexPropsColumnsConj()
-    allProps = basicProps.concat(complexProps, complexPropsColumns);
+    breakUpComplexProps();
+    createVeryComplexProps();
+    allProps = basicProps.concat(complexProps, veryComplexProps);
     numRows = Math.pow(2, basicProps.length);
-    //placeholder
     createRows();
     createAllPropColumns(allProps.length);
     fillAllBasicPropRows(basicProps.length);
